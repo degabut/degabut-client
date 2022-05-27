23a870dfedc42b5b160e5062ea936d0c3464c19e
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-05-27 13:54:31 +0700

    remove unnecessary root element

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -20,40 +20,38 @@ export const App: Component = () => {
 	onCleanup(() => clearInterval(interval));
 
 	return (
-		<>
-			<div class="w-screen h-[calc(100vh-4rem)] lg:h-screen">
-				<div class="flex flex-row h-full">
-					<div class="lg:block hidden">
-						<AppDrawer />
-					</div>
-
-					<div class="h-full flex-grow flex flex-col overflow-y-auto p-3 lg:p-8 pb-48 lg:pb-8 bg-gradient-to-b from-neutral-800 to-neutral-900">
-						<Show
-							when={queueStore.data()}
-							fallback={
-								<div class="flex flex-col flex-grow items-center justify-center w-full h-full space-y-4">
-									{queueStore.data.loading ? (
-										<div class="text-4xl">Loading Queue...</div>
-									) : (
-										<>
-											<div class="text-4xl">No Queue Found :(</div>
-											<div class="text-xl">Join to voice channel where Degabut is</div>
-										</>
-									)}
-								</div>
-							}
-						>
-							<ContextMenuProvider>
-								<Outlet />
-							</ContextMenuProvider>
-						</Show>
-					</div>
+		<div class="w-screen h-[calc(100vh-4rem)] lg:h-screen">
+			<div class="flex flex-row h-full">
+				<div class="lg:block hidden">
+					<AppDrawer />
 				</div>
 
-				<div class="lg:hidden block sticky bottom-0 w-full z-10 h-16">
-					<MobileAppDrawer />
+				<div class="h-full flex-grow flex flex-col overflow-y-auto p-3 lg:p-8 pb-48 lg:pb-8 bg-gradient-to-b from-neutral-800 to-neutral-900">
+					<Show
+						when={!queueStore.data()}
+						fallback={
+							<div class="flex flex-col flex-grow items-center justify-center w-full h-full space-y-4">
+								{queueStore.data.loading ? (
+									<div class="text-4xl">Loading Queue...</div>
+								) : (
+									<>
+										<div class="text-4xl">No Queue Found :(</div>
+										<div class="text-xl">Join to voice channel where Degabut is</div>
+									</>
+								)}
+							</div>
+						}
+					>
+						<ContextMenuProvider>
+							<Outlet />
+						</ContextMenuProvider>
+					</Show>
 				</div>
 			</div>
-		</>
+
+			<div class="lg:hidden block sticky bottom-0 w-full z-10 h-16">
+				<MobileAppDrawer />
+			</div>
+		</div>
 	);
 };
