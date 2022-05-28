dc2d191e9a1b71ec30e717b08d43804bb711fddc
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-05-28 22:25:34 +0700

    add missing padding

--- src/components/AppDrawer/MobileLink.tsx
+++ src/components/AppDrawer/MobileLink.tsx
@@ -14,7 +14,7 @@ export const MobileLink: Component<Props> = ({ icon, label, path }) => {
 	return (
 		<SolidLink
 			href={path}
-			class="flex flex-col flex-grow h-full space-y-1 fill-current items-center justify-center text-center cursor-pointer transition-colors"
+			class="flex flex-col flex-grow h-full space-y-1 py-1.5 fill-current items-center justify-center text-center cursor-pointer transition-colors "
 			classList={{
 				"text-neutral-400 hover:bg-white hover:bg-opacity-5": !isActive(),
 				"text-neutral-100 bg-white bg-opacity-10 font-medium": !!isActive(),
