98b639980686ab55a4ac978de87c1c8825ea5c0a
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-05-29 19:50:30 +0700

    add disabled style

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -10,7 +10,8 @@ export const Input: Component<Props> = (props) => {
 			{...props}
 			class="w-full lg:max-w-[32rem] bg-white rounded-full py-2 px-4 outline-0 text-black"
 			classList={{
-				"rounded-full": props.rounded,
+				"rounded-full": !!props.rounded,
+				"bg-neutral-300 text-neutral-500": !!props.disabled,
 				...props.classList,
 			}}
 		/>
