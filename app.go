c26608431a140d170cf0ce12bef09b485e9f8c05
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-05-31 10:40:59 +0700

    add config file

--- app.go
+++ app.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"os"
 )
+
+var userHomeDir, _ = os.UserHomeDir()
+var configFileName = ".degabut.json"
+var configPath = userHomeDir + "/" + configFileName
+
+type Config struct {
+	AccessToken string `json:"access_token"`
+}
 
 // App struct
 type App struct {
@@ -22,6 +32,29 @@ func (a *App) startup(ctx context.Context) {
 }
 
 // Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *App) SetAccessToken(token string) {
+	// TODO: handle error
+
+	config := Config{
+		AccessToken: token,
+	}
+	configJson, err := json.Marshal(config)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(configPath, configJson, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (a *App) GetAccessToken() string {
+	// read json from file
+	configJson, _ := ioutil.ReadFile(configPath)
+
+	config := Config{}
+	json.Unmarshal([]byte(configJson), &config)
+
+	return config.AccessToken
 }
