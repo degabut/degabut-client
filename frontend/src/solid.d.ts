75b2deea69fbe19a9affeb12114a0d85dbb5deaf
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-05-31 10:47:37 +0700

    refactor debounce

--- frontend/src/solid.d.ts
+++ frontend/src/solid.d.ts
@@ -4,7 +4,6 @@ import { ContextMenuDirectiveParams } from "./providers";
 declare module "solid-js" {
 	namespace JSX {
 		interface Directives {
-			debounce: number;
 			sortable: boolean;
 			clickOutside: () => void;
 			contextMenu: ContextMenuDirectiveParams;
