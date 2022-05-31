cea40a2dd235279c3cbd60bfd12452a0946f6a80
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-05-31 13:23:08 +0700

    add window menu

--- frontend/src/components/AppMenuBar/AppMenuBar.tsx
+++ frontend/src/components/AppMenuBar/AppMenuBar.tsx
@@ -1,10 +1,57 @@
-export const AppMenuBar = () => {
+import { Icon, Icons } from "@components";
+import { doubleClick } from "@directives";
+import { Component, JSX, onMount, splitProps } from "solid-js";
+import { Quit, WindowMaximise, WindowMinimise, WindowUnmaximise } from "../../../wailsjs/runtime/runtime";
+
+doubleClick;
+
+type AppIconProps = JSX.HTMLAttributes<HTMLDivElement> & {
+	icon: Icons;
+};
+
+const AppIcon: Component<AppIconProps> = (props) => {
+	const [{ icon }, rest] = splitProps(props, ["icon"]);
+
 	return (
-		<div class="hidden lg:block h-6 bg-neutral-900">
-			<div class="flex flex-row items-center px-4">
-				<div class="text-sm text-neutral-500 uppercase font-medium">Degabut</div>
-				<div class="flex-grow"></div>
-				<div>{/* RIGHT */}</div>
+		<div class="hover:cursor-pointer hover:bg-white/10 px-4 py-2" {...rest}>
+			<Icon name={icon} extraClass="w-[0.625rem] h-[0.625rem] fill-white" />
+		</div>
+	);
+};
+
+export const AppMenuBar = () => {
+	let isMaximized = false;
+	let isDragging = false;
+
+	const toggleMaximize = () => {
+		console.log("toggling");
+		isMaximized = !isMaximized;
+		isMaximized ? WindowMaximise() : WindowUnmaximise();
+	};
+
+	onMount(() => {
+		document.addEventListener("mousemove", () => {
+			if (isDragging && isMaximized) isMaximized = false;
+		});
+	});
+
+	return (
+		<div
+			class="flex flex-row items-stretch pl-4 bg-neutral-900"
+			onMouseDown={() => (isDragging = true)}
+			onMouseUp={() => (isDragging = false)}
+		>
+			<div
+				class="flex flex-grow text-sm text-neutral-500 uppercase font-medium my-auto"
+				data-wails-drag
+				use:doubleClick={toggleMaximize}
+			>
+				Degabut
+			</div>
+			<div class="flex flex-row items-stretch">
+				<AppIcon icon="minus" onClick={() => WindowMinimise()} />
+				<AppIcon icon="squareLine" onClick={toggleMaximize} />
+				<AppIcon icon="closeLine" onClick={() => Quit()} />
 			</div>
 		</div>
 	);
--- frontend/src/components/Icon/icons/index.ts
+++ frontend/src/components/Icon/icons/index.ts
@@ -1,9 +1,11 @@
 export { default as audioPlaylist } from "./audio-playlist.svg";
+export { default as closeLine } from "./close-line.svg";
 export { default as ellipsis } from "./ellipsis.svg";
 export { default as forward } from "./forward.svg";
 export { default as heart } from "./heart.svg";
 export { default as history } from "./history-line.svg";
 export { default as list } from "./list-square-bullet.svg";
+export { default as minus } from "./minus.svg";
 export { default as musicNote } from "./music-note.svg";
 export { default as plus } from "./plus.svg";
 export { default as search } from "./search.svg";
@@ -11,4 +13,5 @@ export { default as sortArrows } from "./sort-arrows.svg";
 export { default as soundFull } from "./sound-full.svg";
 export { default as soundMedium } from "./sound-medium.svg";
 export { default as soundOff } from "./sound-off.svg";
+export { default as squareLine } from "./square-line.svg";
 export { default as trashBin } from "./trash-bin.svg";
