248b66d680cc7ea3aae08889f20b4b9d6b601f90
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-06-03 00:43:19 +0700

    update rich presence

--- frontend/src/providers/RPCProvider.tsx
+++ frontend/src/providers/RPCProvider.tsx
@@ -17,6 +17,7 @@ export const RPCProvider: ParentComponent = (props) => {
 	if (IS_BROWSER) return props.children;
 
 	let activityUpdaterTimer: NodeJS.Timer;
+	let queue: QueueContextStore | undefined;
 
 	onMount(async () => {
 		const token = await auth.getAccessToken();
@@ -26,23 +27,50 @@ export const RPCProvider: ParentComponent = (props) => {
 		clearInterval(activityUpdaterTimer);
 	});
 
-	const setListeningActivity = async (title: string, channelName: string) => {
-		await rpc.SetActivity({
-			details: title,
-			state: channelName,
-			assets: {
-				large_image: "degabut",
-				large_text: "Degabut",
-			},
-		} as models.rpc.Activity);
+	const updateListeningActivity = async () => {
+		if (!queue) throw new Error("Queue not found");
+		const queueData = queue.data();
+
+		if (!queueData?.nowPlaying) {
+			await rpc.SetActivity({
+				details: "Not listening to anything",
+				state: "💤",
+				assets: {
+					large_image: "degabut",
+					large_text: "Degabut",
+				},
+			} as models.rpc.Activity);
+		} else {
+			const { nowPlaying, voiceChannel } = queueData;
+
+			const title = nowPlaying.video.title;
+			const channelName = nowPlaying.video.channel.name;
+			const otherMemberCount = voiceChannel.members.length - 2;
+			const smallImage = otherMemberCount > 0 ? "multi_user" : "single_user";
+			const smallText =
+				otherMemberCount <= 0
+					? "Listening alone"
+					: `Listening along with ${otherMemberCount} ${otherMemberCount === 1 ? "person" : "people"}`;
+			const start = nowPlaying.playedAt ? Math.floor(new Date(nowPlaying.playedAt).getTime() / 1000) : null;
+
+			await rpc.SetActivity({
+				details: title,
+				state: channelName,
+				assets: {
+					large_image: "degabut",
+					large_text: "Degabut",
+					small_image: smallImage,
+					small_text: smallText,
+				},
+				timestamps: start ? { start } : undefined,
+			} as models.rpc.Activity);
+		}
 	};
 
-	const startActivityUpdater = async (queue: QueueContextStore) => {
-		activityUpdaterTimer = setInterval(() => {
-			const nowPlaying = queue.data()?.nowPlaying;
-			if (!nowPlaying) return;
-			setListeningActivity(nowPlaying.video.title, nowPlaying.video.channel.name);
-		}, 15000);
+	const startActivityUpdater = async (q: QueueContextStore) => {
+		queue = q;
+		activityUpdaterTimer = setInterval(updateListeningActivity, 7500);
+		updateListeningActivity();
 	};
 
 	const store: RPCContextStore = {
