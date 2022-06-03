14b078b04c9280fd383317d8e7821d0e1291ab2f
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-06-03 15:49:15 +0700

    add brand color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
 			screens: {
 				"3xl": "1920px",
 			},
+			colors: {
+				brand: {
+					DEFAULT: "#ECE350",
+					50: "#FEFDF6",
+					100: "#FCFBE3",
+					200: "#F8F5BE",
+					300: "#F4EF9A",
+					400: "#F0E975",
+					500: "#ECE350",
+					600: "#E7DB1D",
+					700: "#B8AE14",
+					800: "#857E0E",
+					900: "#534E09",
+				},
+			},
 		},
 	},
 	plugins: [],
