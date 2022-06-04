39b49827cfb6b82f743d81c948a320ba6b491438
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-06-04 14:05:07 +0700

    limit instance

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"embed"
+	"net"
+	"os"
 
 	"github.com/suspiciouslookingowl/degabut-web/app"
 	"github.com/suspiciouslookingowl/degabut-web/rpc"
@@ -15,12 +18,18 @@ var assets embed.FS
 var clientId string
 
 func main() {
+	// limit to only one instance by locking a tcp port
+	l, err := net.Listen("tcp", "localhost:39821")
+	if err != nil {
+		os.Exit(0)
+	}
+
 	// Create an instance of the app structure
 	app := app.NewApp()
 	rpc, _ := rpc.NewClient(clientId)
 
 	// Create application with options
-	err := wails.Run(&options.App{
+	err = wails.Run(&options.App{
 		Title:     "Degabut",
 		Width:     1048,
 		Height:    768,
@@ -29,6 +38,9 @@ func main() {
 		Frameless: true,
 		Assets:    assets,
 		OnStartup: app.Startup,
+		OnShutdown: func(ctx context.Context) {
+			l.Close()
+		},
 		Bind: []interface{}{
 			app,
 			rpc,
