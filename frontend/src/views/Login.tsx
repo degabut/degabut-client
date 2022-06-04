3089f08542cf16919f73475c37cce1b463c717e8
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-06-04 11:20:28 +0700

    center text

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -27,7 +27,7 @@ export const Login: Component = () => {
 		<div class="h-full flex flex-row items-center justify-center bg-gradient-to-b from-neutral-800 to-neutral-900">
 			<Link
 				href={import.meta.env.VITE_OAUTH_URL}
-				class="border border-neutral-100 px-8 py-4 rounded-lg text-2xl hover:bg-white/10 transition-colors"
+				class="border border-neutral-100 px-8 py-4 rounded-lg text-2xl text-center hover:bg-white/10 transition-colors"
 			>
 				Login with Discord
 			</Link>
