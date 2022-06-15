5f828f3a7d6ec1d6acbdaa4ccd8f2e1dc1070d08
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-06-15 11:27:50 +0700

    change hover styling

--- frontend/src/providers/ContextMenuProvider.tsx
+++ frontend/src/providers/ContextMenuProvider.tsx
@@ -72,7 +72,7 @@ export const ContextMenuProvider: ParentComponent = (props) => {
 					>
 						<For each={params().items}>
 							{(item) => (
-								<div class="cursor-pointer hover:bg-white/10 py-2 px-4" onClick={() => onClick(item)}>
+								<div class="cursor-pointer hover:bg-white/5 py-2 px-4" onClick={() => onClick(item)}>
 									{item.element || item.label}
 								</div>
 							)}
@@ -88,7 +88,7 @@ export const ContextMenuProvider: ParentComponent = (props) => {
 							<For each={params().items}>
 								{(item) => (
 									<div
-										class="cursor-pointer hover:bg-white/10 px-8 py-4"
+										class="cursor-pointer hover:bg-white/5 px-8 py-4"
 										onClick={() => onClick(item)}
 									>
 										{item.element || item.label}
