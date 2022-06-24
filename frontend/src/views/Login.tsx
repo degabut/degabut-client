a6eaba5e86d16c6fbc9e8c0c3c8ce24faf0955c4
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-06-24 11:54:37 +0700

    redesign login

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -24,13 +24,19 @@ export const Login: Component = () => {
 	});
 
 	return (
-		<div class="flex flex-row items-center justify-center h-full">
-			<Link
-				href={import.meta.env.VITE_OAUTH_URL}
-				class="border border-neutral-100 px-8 py-4 rounded-lg text-2xl text-center hover:bg-white/10 transition-colors"
-			>
-				Login with Discord
-			</Link>
+		<div class="flex items-center justify-center h-full">
+			<div class="flex flex-col items-center sm:bg-neutral-900 bg-transparent sm:px-16 sm:py-24 rounded-2xl">
+				<img src="/android-chrome-512x512.png" class="w-32 h-32" />
+				<div class="text-4xl font-brand mt-6 tracking-wide">degabut</div>
+				<div class="mt-24">
+					<Link
+						href={import.meta.env.VITE_OAUTH_URL}
+						class="border border-neutral-100 px-8 py-2 rounded-full text-lg text-center hover:bg-white/10 transition-colors"
+					>
+						Login with Discord
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
